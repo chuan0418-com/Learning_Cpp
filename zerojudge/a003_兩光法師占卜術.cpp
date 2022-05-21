c837f441af5146b4a820729a7619978609111a6c
銓chuan <admin@chuan0418.com>
2022-05-21 11:12:25 +0800

    update

---
+++ zerojudge/a003_兩光法師占卜術.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, D, S;
+    cin >> M;
+    cin >> D;
+    S=(M*2+D)%3;
+    if (S==0){
+        cout << "普通";
+    }
+    if (S==1){
+        cout << "吉";
+    }
+    if (S==2){
+        cout << "大吉";
+    }
+}
